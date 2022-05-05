2e2550d97879de32349b24cf3945e513fe0f1c9e
hunterkoppenmansystems <hunter.koppen@mansystems.nl>
2022-05-05 11:30:43 +0200

    first commit

---
+++ src/TabSwitch.jsx
@@ -0,0 +1,7 @@
+import { createElement } from "react";
+
+import { TabSwitchFunction } from "./components/TabSwitchFunction";
+
+export function TabSwitch({ tabIndex }) {
+    return <TabSwitchFunction tabIndex={tabIndex} />;
+}
